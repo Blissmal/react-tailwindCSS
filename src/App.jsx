41de5914325ef1d3e9a6b1db36217f0b49583e17
Blissmal <malutibethuel@gmail.com>
2024-07-24 18:51:42 +0300

    tailwind learning project

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Hero from "./sections/Hero.jsx";
+import PopularProducts from "./sections/PopularProducts.jsx";
+import SuperQuality from "./sections/SuperQuality.jsx";
+import Services from "./sections/Services.jsx";
+import SpecialOffer from "./sections/SpecialOffer.jsx";
+import CustomerReviews from "./sections/CustomerReviews.jsx";
+import Subscribe from "./sections/Subscribe.jsx";
+import Footer from "./sections/Footer.jsx";
+import Nav from "./components/Nav.jsx";
+
+const App = () => (
+    <main className='relative'>
+        <Nav />
+        <section className="xl:padding-l wide:padding-r padding-b">
+            <Hero />
+            <section className="padding">
+                <PopularProducts />
+            </section>
+            <section className="padding">
+                <SuperQuality />
+            </section>
+            <section className="padding-x padding-y-10">
+                <Services />
+            </section>
+            <section className="padding">
+                <SpecialOffer />
+            </section>
+            <section className="bg-pale-blue padding">
+                <CustomerReviews />
+            </section>
+            <section className="padding-x sm:py-32 py-16 w-full">
+                <Subscribe />
+            </section>
+            <section className="bg-black padding-x padding-t pb-8">
+                <Footer />
+            </section>
+        </section>
+    </main>
+)
+export default App
